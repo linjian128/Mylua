72a48641db39425130af09edb7dc61536edd8124
linjian128 <linjian128@gmail.com>
2018-10-10 14:51:25 +0800

    Update mian.lua

--- mian.lua
+++ mian.lua
@@ -1,3 +1,9 @@
+print('Setting up WIFI...')
+wifi.setmode(wifi.STATION)
+wifi.sta.config('EmployeeHotspot', 'abc123bcd')
+wifi.sta.connect()
+
+
 tmr.alarm(0, 1000, 1, function()
    if wifi.sta.getip() == nil then
       print("Connecting to AP...")
